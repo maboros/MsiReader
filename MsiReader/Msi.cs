0fc17207a1bd2d70bfd7e2c5157cb62f90816f48
Marko Boroš <maboros@vsite.hr>
2021-07-24 20:31:49 +0200

    Add Msi. class separation and protection for direct calls of dll functions

---
+++ MsiReader/Msi.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MsiReader
+{
+    public static class Msi
+    {
+        [DllImport("msi.dll", SetLastError = true)]
+        static extern uint MsiOpenDatabase(string szDatabasePath, IntPtr phPersist, out IntPtr phDatabase);
+
+        [DllImport("msi.dll", CharSet = CharSet.Unicode)]
+        static extern int MsiDatabaseOpenView(IntPtr hDatabase, [MarshalAs(UnmanagedType.LPWStr)] string szQuery, out IntPtr phView);
+
+        [DllImport("msi.dll", CharSet = CharSet.Unicode)]
+        static extern int MsiViewExecute(IntPtr hView, IntPtr hRecord);
+
+        [DllImport("msi.dll", CharSet = CharSet.Unicode)]
+        static extern uint MsiViewFetch(IntPtr hView, out IntPtr hRecord);
+
+
+        [DllImport("msi.dll", CharSet = CharSet.Unicode)]
+        static extern int MsiRecordGetString(IntPtr hRecord, int iField, StringBuilder szValueBuf, ref int pcchValueBuf);
+
+        [DllImport("msi.dll", ExactSpelling = true)]
+        static extern int MsiRecordDataSize(IntPtr hRecord, int iField);
+
+        const int MSICOLINFO_NAMES = 0;  // return column names
+        const int MSICOLINFO_TYPES = 1;  // return column definitions, datatype code followed by width
+        [DllImport("msi.dll", ExactSpelling = true)]
+        static extern uint MsiViewGetColumnInfo(IntPtr hView, int eColumnInfo, out IntPtr hRecord);
+
+        [DllImport("msi.dll", ExactSpelling = true)]
+        static extern int MsiRecordGetFieldCount(IntPtr hRecord);
+
+        public static uint OpenDatabase(string szDatabasePath, IntPtr phPersist, out IntPtr phDatabase)
+        {
+            return MsiOpenDatabase(szDatabasePath, phPersist, out phDatabase);
+        }
+        public static int DatabaseOpenView(IntPtr hDatabase, [MarshalAs(UnmanagedType.LPWStr)] string szQuery, out IntPtr hView)
+        {
+            return MsiDatabaseOpenView(hDatabase,szQuery, out hView);
+        }
+        public static int ViewExecute(IntPtr hView, IntPtr hRecord)
+        {
+            return MsiViewExecute(hView, IntPtr.Zero);
+        }
+        public static uint ViewFetch(IntPtr hView, out IntPtr hRecord)
+        {
+            return MsiViewFetch(hView,out hRecord);
+        }
+        public static int RecordGetString(IntPtr hRecord, int iField, StringBuilder szValueBuf, ref int pcchValueBuf)
+        {
+            return MsiRecordGetString(hRecord,iField,szValueBuf,ref pcchValueBuf);
+        }
+        public static uint ViewGetColumnInfo(IntPtr hView, int eColumnInfo, out IntPtr hRecord)
+        {
+            return MsiViewGetColumnInfo(hView,eColumnInfo,out hRecord);
+        }
+        public static int RecordGetFieldCount(IntPtr hRecord)
+        {
+            return MsiRecordGetFieldCount(hRecord);
+        }
+        public static int RecordDataSize(IntPtr hRecord, int iField)
+        {
+            return MsiRecordDataSize(hRecord,iField);
+        }
+    }
+}
--- MsiReader/Reader.cs
+++ MsiReader/Reader.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using OpenMcdf;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Text;
 using OpenMcdf.Extensions;
 using OpenMcdf.Extensions.OLEProperties;
@@ -27,57 +26,30 @@ namespace MsiReader
     }
     public class MsiPull
     {
-        [DllImport("msi.dll", SetLastError = true)]
-        static extern uint MsiOpenDatabase(string szDatabasePath, IntPtr phPersist, out IntPtr phDatabase);
-
-        [DllImport("msi.dll", CharSet = CharSet.Unicode)]
-        static extern int MsiDatabaseOpenView(IntPtr hDatabase, [MarshalAs(UnmanagedType.LPWStr)] string szQuery, out IntPtr phView);
-
-        [DllImport("msi.dll", CharSet = CharSet.Unicode)]
-        static extern int MsiViewExecute(IntPtr hView, IntPtr hRecord);
-
-        [DllImport("msi.dll", CharSet = CharSet.Unicode)]
-        static extern uint MsiViewFetch(IntPtr hView, out IntPtr hRecord);
-
-
-        [DllImport("msi.dll", CharSet = CharSet.Unicode)]
-        static extern int MsiRecordGetString(IntPtr hRecord, int iField, StringBuilder szValueBuf, ref int pcchValueBuf);
-
-        [DllImport("msi.dll", ExactSpelling = true)]
-        static extern int MsiRecordDataSize(IntPtr hRecord, int iField);
-
-        const int MSICOLINFO_NAMES = 0;  // return column names
-        const int MSICOLINFO_TYPES = 1;  // return column definitions, datatype code followed by width
-        [DllImport("msi.dll", ExactSpelling = true)]
-        static extern uint MsiViewGetColumnInfo(IntPtr hView, int eColumnInfo, out IntPtr hRecord);
-
-        [DllImport("msi.dll", ExactSpelling = true)]
-        static extern int MsiRecordGetFieldCount(IntPtr hRecord);
-
         public static int DrawFromMsi(String fileName,ref List<String> returnNames)
         {
             try
             {
-                if (MsiOpenDatabase(fileName, IntPtr.Zero, out IntPtr hDatabase) != Win32Error.NO_ERROR)
+                if (Msi.OpenDatabase(fileName, IntPtr.Zero, out IntPtr hDatabase) != Win32Error.NO_ERROR)
                 {
                     Console.WriteLine(fileName);
                     Console.WriteLine("Failed to open dabase");
                     return 1;
                 }
 
-                if (MsiDatabaseOpenView(hDatabase, "SELECT `Name` FROM _Tables", out IntPtr hView) != Win32Error.NO_ERROR)
+                if (Msi.DatabaseOpenView(hDatabase, "SELECT `Name` FROM _Tables", out IntPtr hView) != Win32Error.NO_ERROR)
                 {
                     Console.WriteLine("Failed to open view");
                     return 1;
                 }
 
-                MsiViewExecute(hView, IntPtr.Zero);
+                Msi.ViewExecute(hView, IntPtr.Zero);
 
-                while (MsiViewFetch(hView, out IntPtr hRecord) != Win32Error.ERROR_NO_MORE_ITEMS)
+                while (Msi.ViewFetch(hView, out IntPtr hRecord) != Win32Error.ERROR_NO_MORE_ITEMS)
                 {
                     StringBuilder buffer = new StringBuilder(256);
                     int capacity = buffer.Capacity;
-                    if (MsiRecordGetString(hRecord, 1, buffer, ref capacity) != Win32Error.NO_ERROR)
+                    if (Msi.RecordGetString(hRecord, 1, buffer, ref capacity) != Win32Error.NO_ERROR)
                     {
                         Console.WriteLine("Failed to get record string");
                         return 1;
@@ -96,40 +68,38 @@ namespace MsiReader
         {
             try
             {
-                if (MsiOpenDatabase(fullPathName, IntPtr.Zero, out IntPtr hDatabase) != Win32Error.NO_ERROR)
+                if (Msi.OpenDatabase(fullPathName, IntPtr.Zero, out IntPtr hDatabase) != Win32Error.NO_ERROR)
                 {
                     Console.WriteLine(fullPathName);
                     return 1;
                 }
 
-                if (MsiDatabaseOpenView(hDatabase, $"SELECT * FROM `{tableName}`", out IntPtr hView) != Win32Error.NO_ERROR)
+                if (Msi.DatabaseOpenView(hDatabase, $"SELECT * FROM `{tableName}`", out IntPtr hView) != Win32Error.NO_ERROR)
                 {
                     return 2;
                 }
-                MsiViewExecute(hView, IntPtr.Zero);
-                while (MsiViewFetch(hView, out IntPtr hRecord) != Win32Error.ERROR_NO_MORE_ITEMS)
+                Msi.ViewExecute(hView, IntPtr.Zero);
+                while (Msi.ViewFetch(hView, out IntPtr hRecord) != Win32Error.ERROR_NO_MORE_ITEMS)
                 {
                     // dohvaća informacije o imenima stupaca; ako je drugi argument 1, vratit će 
                     // informacije o tipu u pojedinim stupcima (https://docs.microsoft.com/en-us/windows/win32/msi/column-definition-format)
-                    MsiViewGetColumnInfo(hView, 0, out IntPtr hColumnRecord);
+                    Msi.ViewGetColumnInfo(hView, 0, out IntPtr hColumnRecord);
                     // dohvaća broj stupaca
-                    var fieldCount = MsiRecordGetFieldCount(hRecord);
+                    var fieldCount = Msi.RecordGetFieldCount(hRecord);
                     // za svaki stupac...
                     for (int i = 0; i < fieldCount; ++i)
                     {
                         // ..dohvaća duljinu imena...
-                        var dataSize = MsiRecordDataSize(hColumnRecord, i + 1);
+                        var dataSize = Msi.RecordDataSize(hColumnRecord, i + 1);
                         // ...i samo ime
                         StringBuilder buffer = new StringBuilder(dataSize + 1);
                         int capacity = buffer.Capacity;
-                        if (MsiRecordGetString(hColumnRecord, i + 1, buffer, ref capacity) != Win32Error.NO_ERROR)
+                        if (Msi.RecordGetString(hColumnRecord, i + 1, buffer, ref capacity) != Win32Error.NO_ERROR)
                         {
                             return 3;
                         }
                         dataList.Add(buffer.ToString());
                     }
-                   
-
                 }
                 return 0;
             }
