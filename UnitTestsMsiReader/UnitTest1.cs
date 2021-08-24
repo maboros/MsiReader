45f8366734950532c64b7e7cca6ad8f82fd83cd9
Marko Boroš <maboros@vsite.hr>
2021-08-24 15:27:46 +0200

    Adjust naming for test

--- UnitTestsMsiReader/UnitTest1.cs
+++ UnitTestsMsiReader/UnitTest1.cs
@@ -9,32 +9,32 @@ namespace UnitTestsMsiReader
 {
     public class Tests
     {
-        string solutiondir;
+        string projectdir;
         [SetUp]
         public void Setup()
         {
-            solutiondir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            projectdir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
         }
 
         [Test]
         public void FileIsLoadedAndDrawFromMsiReturns0()
         {
             List<String> allFileNames = new List<String>();
-            Assert.AreEqual(0, MsiPull.DrawFromMsi(solutiondir + "\\" + "appData" + "\\68b3ac.msi", ref allFileNames));
+            Assert.AreEqual(0, MsiPull.DrawFromMsi(projectdir + "\\" + "appData" + "\\68b3ac.msi", ref allFileNames));
         }
 
         [Test]
         public void FirstItemInDrawFromMsiIsCorrect()
         {
             List<String> allFileNames = new List<String>();
-            MsiPull.DrawFromMsi(solutiondir + "\\" + "appData" + "\\68b3ac.msi", ref allFileNames);
+            MsiPull.DrawFromMsi(projectdir + "\\" + "appData" + "\\68b3ac.msi", ref allFileNames);
             Assert.AreEqual("_Validation", allFileNames.First());
         }
 
         [Test]
         public void GetSummaryInformationPullsFirstItem()
         {
-            var list = MsiPull.getSummaryInformation(solutiondir + "\\" + "appData" + "\\68b3ac.msi");
+            var list = MsiPull.getSummaryInformation(projectdir + "\\" + "appData" + "\\68b3ac.msi");
             Assert.AreEqual("CodePageString", list.First().name);
             Assert.AreEqual("1252", list.First().value);
         }
